26d9e8b6be137cecd737112b2c32899abf45caf9
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-08-31 13:36:03 +0300

    editing

--- javascript-basics-01/Step_3.js
+++ javascript-basics-01/Step_3.js
@@ -1,19 +1,3 @@
+var userInput = prompt("Please enter your name:");
 
-
-    document.getElementById("dialog").style.display = "block";
-
-  
-
-  document.getElementById("closeDialog").addEventListener("click", function() {
-    document.getElementById("dialog").style.display = "none";
-  });
-  
-
-  document.getElementById("submitName").addEventListener("click", function() {
-    const nameInput = document.getElementById("nameInput").value;
-    if (nameInput) {
-      alert("Hello, " + nameInput + "!");
-      document.getElementById("dialog").style.display = "none";
-    }
-  });
-  
+alert( "Hello," + userInput  + "!" );
