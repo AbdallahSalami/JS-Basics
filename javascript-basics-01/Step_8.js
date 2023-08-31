4f350901c93231ad65a66daefaf4971083f017b3
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-08-31 15:44:40 +0300

    adding Eighth JS file

---
+++ javascript-basics-01/Step_8.js
@@ -0,0 +1,20 @@
+
+
+
+
+
+
+document.getElementById("validate").addEventListener("click", function() {
+    var ageInput= document.getElementById("age").value ;
+    if(ageInput > 18) {
+         alert("you are over 18");
+
+    }
+
+    else {
+
+         alert("you are under 18");
+
+        }
+    })
+ 
