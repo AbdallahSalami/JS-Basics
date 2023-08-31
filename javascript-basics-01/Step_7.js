5d342cc2e8a28f48c7908807fce9095e4c50b5c9
AbdallahSalami <AbdallahSalami@users.noreply.github.com>
2023-08-31 15:34:21 +0300

    adding Eighth JS file

---
+++ javascript-basics-01/Step_7.js
@@ -0,0 +1,11 @@
+
+
+
+
+document.getElementById("validate").addEventListener("click", function() {
+    var shoe_sizeNumber = document.getElementById("shoe_size").value ;
+    var yearNumber = document.getElementById("year").value ;
+    
+
+  alert( "Multiplication= " + ((shoe_sizeNumber *2) + (5)  *50 -(yearNumber) + 1766 +   "!" ));
+});
